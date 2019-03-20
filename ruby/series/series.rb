75aea43a91b98cd8835140876d5a6088349112ba
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-03-20 14:43:16 -0600

    Finish series challenge

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -10,8 +10,14 @@ class Series
     if length > numstring.length
       raise ArgumentError
     else
-      binding.pry
-      numstring.slice!(length)
+      rounds = numstring.length - (length - 1)
+      final = []
+      index = 0
+      until index == rounds do
+        final << numstring[index, length]
+        index = index + 1
+      end
+      return final
     end
   end
 
