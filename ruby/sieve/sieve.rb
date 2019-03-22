3296d06622390df6e93ccfeee3f33d6e6cd9be33
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-03-22 16:03:04 -0600

    Add base files and begin Sieve class

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,16 @@
+class Sieve
+  def initialize(limit)
+    @limit = limit
+    @primes = [2,3]
+  end
+
+  def primes
+    prime_nums = []
+    marked = []
+    ranger = [2..limit]
+    pointer = 2
+
+    return final
+  end
+
+end
