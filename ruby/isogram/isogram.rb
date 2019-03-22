6e2462547e88a3f102476a8baa83ff2fa5ebc6b5
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-03-21 22:30:23 -0600

    Finish isogram exercise

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -4,13 +4,13 @@ class Isogram
 
   def self.isogram?(input)
     final = true
-    tracker = input.gsub(/(\s-)/, '').downcase.split('').inject(Hash.new(0)) do |memo, letter|
+    tracker = input.gsub(/[\s-]/, '').downcase.split('')
+    tracker.inject(Hash.new(0)) do |memo, letter|
       return final if final == false
       memo[letter] += 1
       final = false if memo[letter] > 1
       memo
     end
-    # binding.pry
     return final
   end
 
